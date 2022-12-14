a3ee828325394e77890a296111797627d5694abf
a-horse-with-no-name <alyabra.vargas@gmail.com>
2022-12-14 16:26:57 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navar from "./components/Navar"
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navar.css'
+
+const Navar = () => {
+  return (
+    <nav>
+        About me
+    </nav>
+  )
+}
+
+export default Navar
